7b0520db88e6709f3e6211ac9373b1a73f7f2002
ChangSubKwak <paradise1732@gmail.com>
2021-11-19 18:24:10 +0900

    feat: exercise to how to convert type

---
+++ src/chap02/section3/TypeConvert.kt
@@ -0,0 +1,11 @@
+package chap02.section3
+
+fun main() {
+    val a: Int = 1
+//    val b: Double = a       // Error
+//    val c: Int = 1.1        // Error
+
+    val d: Double = a.toDouble()
+    val result = 1L + 3       // Long
+
+}
