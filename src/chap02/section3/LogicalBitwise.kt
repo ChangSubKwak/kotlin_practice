cfcbfae37d8fc39c3f6c230f5f823c2ae679040a
ChangSubKwak <paradise1732@gmail.com>
2021-11-23 11:55:19 +0900

    feat: study 'or', 'and'

---
+++ src/chap02/section3/LogicalBitwise.kt
@@ -0,0 +1,13 @@
+package chap02.section3
+
+fun main() {
+    val num1 = 12
+    val num2 = 25
+    var result: Int
+
+    result = num1 or num2
+    println("result : $result")
+
+    result = num1 and num2
+    println("result : $result")
+}
