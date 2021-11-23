bbf7f8efbebd2bc93a56174a12f3e22e19a51d2b
ChangSubKwak <paradise1732@gmail.com>
2021-11-23 11:53:14 +0900

    feat: study bit function

---
+++ src/chap02/section3/BitsShift.kt
@@ -0,0 +1,26 @@
+package chap02.section3
+
+fun main() {
+    var x = 4                   // 4
+    var y = 0b0000_1010         // 5
+    var z = 0x0F                // 15
+
+    println("x shl 2 -> ${x shl 2}")
+    println("x shl 2 -> " + x.shl(2))
+
+    println("x inv() -> ${x.inv()}")
+    println("x inv() -> " + x.inv())
+
+    println("y shr 2 -> ${y/4}, ${y shr 2}")
+    println("y shr 2 -> " + y/4 + ", " + y.shr(2))
+
+    println("x shl 4 -> ${x*16}, ${x shl 4}")
+    println("x shl 4 -> " + x*16 + ", " + x.shl(4))
+
+    println("z shl 4 -> ${z*16}, ${z shl 4}")
+    println("z shl 4 -> " + z*16 + ", " + z.shl(4))
+
+    x = 64
+    println("x shr 4 -> ${x/4}, ${x shr 2}")
+    println("x shr 4 -> " + x/4 + ", " + x.shr(2))
+}
