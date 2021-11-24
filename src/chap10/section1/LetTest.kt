82e1720c01b4bca81a3516aa5d480c48d8906045
ChangSubKwak <paradise1732@gmail.com>
2021-11-24 14:41:47 +0900

    feat: study kotlin standard function 'let'

---
+++ src/chap10/section1/LetTest.kt
@@ -0,0 +1,59 @@
+package chap10.section1
+
+fun main() {
+    val score: Int? = 32
+    // var score = null
+
+    // 일반적인 null 검사
+    fun checkScore() {
+        if (score != null) {
+            println("Score: $score")
+        }
+    }
+
+    // let함수 사용하여 null 검사를 제거
+    fun checkScoreLet() {
+        score?.let {
+            println("Score: $it")
+        }
+        val str = score.let { it.toString() }
+        println(str)
+    }
+    checkScore()
+    checkScoreLet()
+
+    var obj: String?
+    obj = "NotNull"
+    if (null != obj) {
+        println("This is not null")
+    }
+
+    obj?.let {
+        println("This is not null --> $it")
+    }
+
+    var firstName: String?
+    var lastName: String
+    firstName = "changsub"
+    lastName = "kwak"
+    if (null != firstName) {
+        println("$firstName $lastName")
+    } else {
+        println("$lastName")
+    }
+
+    firstName = null
+    firstName?.let { println("$it $lastName") } ?: println("$lastName")
+
+    var a = 1
+    var b = 2
+
+    a = a.let { it + 2 }.let {
+        val i = it + b
+        i
+    }
+    println("a = $a")       // a = 5
+
+    // let은 가독성을 위해 사용하는 것을 권장하지 않음
+}
+
